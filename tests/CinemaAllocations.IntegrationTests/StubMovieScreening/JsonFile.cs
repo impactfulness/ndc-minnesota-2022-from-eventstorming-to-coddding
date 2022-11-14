fe018f6291134344531c431efe36804a6f5b4714
João Rosa <joaoasrosa@gmail.com>
2022-11-14 17:34:33 -0600

    Port code for old solution

---
+++ tests/CinemaAllocations.IntegrationTests/StubMovieScreening/JsonFile.cs
@@ -0,0 +1,20 @@
+using Newtonsoft.Json;
+
+namespace CinemaAllocations.IntegrationTests.StubMovieScreening;
+
+public static class JsonFile
+{
+    public static T ReadFromJsonFile<T>(string filePath) where T : new()
+    {
+        TextReader reader = new StreamReader(filePath);
+        try
+        {
+            var fileContents = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<T>(fileContents);
+        }
+        finally
+        {
+            reader.Close();
+        }
+    }
+}
