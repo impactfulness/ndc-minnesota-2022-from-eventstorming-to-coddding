e66e18c33edb67c324a27089e84f8244856ca047
João Rosa <joaoasrosa@gmail.com>
2022-11-14 16:55:54 -0600

    Revert lab3begin branch to the correct state

--- src/CinemaAllocations.Adapters.DataPersistence/MovieScreening.cs
+++ src/CinemaAllocations.Adapters.DataPersistence/MovieScreening.cs
@@ -2,33 +2,8 @@ namespace CinemaAllocations.Adapters.DataPersistence;
 
 public class MovieScreening
 {
-    public string Id { get; set; }
-
-    public virtual List<Row> Rows { get; set; }
-
-    public Domain.MovieScreening ToDomainModel()
+    internal Domain.MovieScreening ToDomainModel()
     {
-        var rows = new Dictionary<string, Domain.Row>(Rows.Count);
-
-        foreach (var rowDataModel in Rows)
-        {
-            var seats = new List<Domain.Seat>(rowDataModel.Seats.Count);
-
-            foreach (var seatDataModel in rowDataModel.Seats)
-            {
-                Enum.TryParse(seatDataModel.Availability, out SeatAvailability seatAvailability);
-
-                var seat = new Domain.Seat(
-                    rowDataModel.Name,
-                    seatDataModel.Number,
-                    seatAvailability);
-
-                seats.Add(seat);
-            }
-
-            rows.Add(rowDataModel.Name, new Domain.Row(rowDataModel.Name, seats));
-        }
-
-        return new Domain.MovieScreening(rows);
+        throw new NotImplementedException();
     }
 }
--- src/CinemaAllocations.Adapters.DataPersistence/MovieScreeningRepository.cs
+++ src/CinemaAllocations.Adapters.DataPersistence/MovieScreeningRepository.cs
@@ -2,24 +2,10 @@ using CinemaAllocations.Domain;
 
 namespace CinemaAllocations.Adapters.DataPersistence;
 
-public class MovieScreeningRepository : IMovieScreeningRepository, IDisposable
+public class MovieScreeningRepository : IMovieScreeningRepository
 {
-    private readonly CinemaContext _cinemaContext;
-
-    public MovieScreeningRepository(CinemaContext cinemaContext)
-    {
-        _cinemaContext = cinemaContext ?? throw new ArgumentNullException(nameof(cinemaContext));
-    }
-
     public Domain.MovieScreening FindMovieScreeningById(string screeningId)
     {
-        var movieScreeningDataModel = _cinemaContext.MovieScreenings.SingleOrDefault(x => x.Id == screeningId);
-
-        return movieScreeningDataModel?.ToDomainModel();
-    }
-
-    public void Dispose()
-    {
-        _cinemaContext?.Dispose();
+        throw new NotImplementedException();
     }
 }
