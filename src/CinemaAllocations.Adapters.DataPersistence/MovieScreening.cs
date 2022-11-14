8694e425ae2961603c529affa2e1eb16751964fc
João Rosa <joaoasrosa@gmail.com>
2022-11-14 23:48:44 +0100

    Fix porting of class

--- src/CinemaAllocations.Adapters.DataPersistence/MovieScreening.cs
+++ src/CinemaAllocations.Adapters.DataPersistence/MovieScreening.cs
@@ -2,36 +2,33 @@ namespace CinemaAllocations.Adapters.DataPersistence;
 
 public class MovieScreening
 {
-    public class MovieScreening
+    public string Id { get; set; }
+
+    public virtual List<Row> Rows { get; set; }
+
+    public Domain.MovieScreening ToDomainModel()
     {
-        public string Id { get; set; }
+        var rows = new Dictionary<string, Domain.Row>(Rows.Count);
 
-        public virtual List<Row> Rows { get; set; }
-
-        public Domain.MovieScreening ToDomainModel()
+        foreach (var rowDataModel in Rows)
         {
-            var rows = new Dictionary<string, Domain.Row>(Rows.Count);
+            var seats = new List<Domain.Seat>(rowDataModel.Seats.Count);
 
-            foreach (var rowDataModel in Rows)
+            foreach (var seatDataModel in rowDataModel.Seats)
             {
-                var seats = new List<Domain.Seat>(rowDataModel.Seats.Count);
+                Enum.TryParse(seatDataModel.Availability, out SeatAvailability seatAvailability);
 
-                foreach (var seatDataModel in rowDataModel.Seats)
-                {
-                    Enum.TryParse(seatDataModel.Availability, out SeatAvailability seatAvailability);
+                var seat = new Domain.Seat(
+                    rowDataModel.Name,
+                    seatDataModel.Number,
+                    seatAvailability);
 
-                    var seat = new Domain.Seat(
-                        rowDataModel.Name,
-                        seatDataModel.Number,
-                        seatAvailability);
-
-                    seats.Add(seat);
-                }
-
-                rows.Add(rowDataModel.Name, new Domain.Row(rowDataModel.Name, seats));
+                seats.Add(seat);
             }
 
-            return new Domain.MovieScreening(rows);
+            rows.Add(rowDataModel.Name, new Domain.Row(rowDataModel.Name, seats));
         }
+
+        return new Domain.MovieScreening(rows);
     }
 }
