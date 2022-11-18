2073d2373d3072006b15e5383ba9f197e05654d3
João Rosa <joaoasrosa@gmail.com>
2022-11-18 13:17:36 -0600

    Assign constructor variable

--- src/CinemaAllocations.Domain/Rows.cs
+++ src/CinemaAllocations.Domain/Rows.cs
@@ -4,9 +4,11 @@ namespace CinemaAllocations.Domain
 {
     public class Rows
     {
+        private readonly Dictionary<string, Row> _rows;
+
         public Rows(Dictionary<string,Row> rows)
         {
-            
+            _rows = rows;
         }
     }
 }
