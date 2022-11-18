a4b98050d3335cdf70852833fc544f7d7c7abbfd
João Rosa <joaoasrosa@gmail.com>
2022-11-18 16:33:31 -0600

    Add IEquatable for the tiny type

--- src/CinemaAllocations.Domain/SeatNumber.cs
+++ src/CinemaAllocations.Domain/SeatNumber.cs
@@ -1,6 +1,22 @@
+using System;
+
 namespace CinemaAllocations.Domain
 {
-    public struct SeatNumber
+    public struct SeatNumber: IEquatable<SeatNumber>
     {
+        public bool Equals(SeatNumber other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is SeatNumber other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
