127468e25a124e3a632d6b8b0e29a0be4bc1a6ba
João Rosa <joaoasrosa@gmail.com>
2022-11-18 16:06:47 -0600

    Add constructor for row name

--- src/CinemaAllocations.Domain/RowName.cs
+++ src/CinemaAllocations.Domain/RowName.cs
@@ -1,6 +1,32 @@
+using System;
+
 namespace CinemaAllocations.Domain
 {
-    public struct RowName
+    public struct RowName : IEquatable<RowName>
     {
+        private readonly string _rowName;
+
+        private RowName(string rowName)
+        {
+            if (string.IsNullOrWhiteSpace(rowName))
+                throw new ArgumentException("Row name cannot be null or whitespace.", nameof(rowName));
+            
+            _rowName = rowName;
+        }
+
+        public bool Equals(RowName other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RowName other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
