3d53b0808ae4f991716bf5148f092f02ee62aa4e
João Rosa <joaoasrosa@gmail.com>
2022-11-18 16:35:51 -0600

    Implement tiny type

--- src/CinemaAllocations.Domain/SeatNumber.cs
+++ src/CinemaAllocations.Domain/SeatNumber.cs
@@ -2,21 +2,59 @@ using System;
 
 namespace CinemaAllocations.Domain
 {
-    public struct SeatNumber: IEquatable<SeatNumber>
+    public struct SeatNumber : IEquatable<SeatNumber>
     {
+        private readonly uint _value;
+
+        private SeatNumber(uint seatNumber)
+        {
+            if (seatNumber == 0)
+                throw new ArgumentOutOfRangeException(nameof(seatNumber), seatNumber, "Seat number cannot be zero.");
+
+            _value = seatNumber;
+        }
+
+        public static implicit operator uint(SeatNumber seatNumber)
+        {
+            return seatNumber._value;
+        }
+
+        public static explicit operator SeatNumber(uint seatNumber)
+        {
+            return new SeatNumber(seatNumber);
+        }
+
         public bool Equals(SeatNumber other)
         {
-            throw new NotImplementedException();
+            return _value == other._value;
         }
 
         public override bool Equals(object? obj)
         {
+            if (obj == null)
+                return false;
+
             return obj is SeatNumber other && Equals(other);
         }
 
         public override int GetHashCode()
         {
-            throw new NotImplementedException();
+            return _value.GetHashCode();
+        }
+
+        public static bool operator ==(SeatNumber left, SeatNumber right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(SeatNumber left, SeatNumber right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
         }
     }
 }
